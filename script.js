cfcace5ba084aaa4dff8e5feaa3a7bab9415e646
theotrain <owen@localhost-2.local>
2023-01-08 15:44:35 -0800

    first

---
+++ script.js
@@ -0,0 +1,18 @@
+// sheetID you can find in the URL of your spreadsheet after "spreadsheet/d/"
+const sheetId = "1iUWlbfIS0ZGcl1aL5G5xTN-xaCm1hqCPVJRvSn4LDQ0";
+// sheetName is the name of the TAB in your spreadsheet (default is "Sheet1")
+const sheetName = encodeURIComponent("Class Data");
+const sheetURL = `https://docs.google.com/spreadsheets/d/${sheetId}/gviz/tq?tqx=out:csv&sheet=${sheetName}`;
+
+$.ajax({
+  type: "GET",
+  url: sheetURL,
+  dataType: "text",
+  success: function (response) {
+    // var data = $.csv.toArrays(response);
+    var data = $.csv.toObjects(response);
+    console.log(data);
+    // 'data' is an Array of Objects
+    // ADD YOUR CODE HERE
+  },
+});
